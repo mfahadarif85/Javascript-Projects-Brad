97e1b4a26e8545db4be99ec470c62505c645c433
mfahadarif85 <mfahadarif@gmail.com>
2022-06-11 11:38:26 +0500

    added js code

--- Hangman/script.js
+++ Hangman/script.js
@@ -4,6 +4,7 @@ const wrongLettersEl = document.getElementById("wrong-letters");
 const finalMsgEl = document.getElementById("final-message");
 const playBtn = document.getElementById("play-button");
 const notificationEl = document.getElementById("notification-container");
+const figurePartsEl = document.querySelectorAll("figure-part");
 
 const wordsArr = [
   "apple",
@@ -41,5 +42,40 @@ function displayWord() {
     popupEl.style.display = "flex";
   }
 }
+
+//show notification for double key press
+function showNotification() {
+  notificationEl.classList.add("show");
+  setTimeout(() => {
+    notificationEl.classList.remove("show");
+  }, 2500);
+}
+
+// update the wrong letters element
+function updateWrongLettersEl() {
+  console.log("update wrong letters element");
+}
+
+// key down event listener
+window.addEventListener("keydown", (e) => {
+  if (e.key.charCodeAt() >= 97 && e.key.charCodeAt() <= 122) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
