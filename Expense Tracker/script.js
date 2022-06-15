b6761529ad0d1bb07796fc636eef808b9ae0c01c
mfahadarif85 <mfahadarif@gmail.com>
2022-06-15 14:04:11 +0500

    js complete

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -6,17 +6,55 @@ const formEl = document.getElementById("form");
 const textEl = document.getElementById("text");
 const amountEl = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 1, text: "Flower", amount: 150 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Profit", amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+// get local storage transactions and save to transactions array
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
-// add transactions to DOM list
+//function to update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
 
+// function to add new transaction to history list
+function addTransaction(e) {
+  //prevent submission of form with page refresh
+  e.preventDefault();
+  //alert if no values are input
+  if (text.value.trim() === "" || amountEl.value.trim() === "") {
+    alert("Please add a text and amount");
+  }
+  // update inputs in transactionss
+  else {
+    const transaction = {
+      id: generateID(),
+      text: textEl.value,
+      amount: +amountEl.value,
+    };
+    transactions.push(transaction);
+    addTransactionDom(transaction);
+    updateValues();
+    updateLocalStorage();
+    textEl.value = "";
+    amountEl.value = "";
+  }
+}
+
+// function to generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+// function to add transactions to DOM list
 function addTransactionDom(transaction) {
   // get sign
   const sign = transaction.amount < 0 ? "-" : "+";
@@ -29,12 +67,21 @@ function addTransactionDom(transaction) {
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
-  )}</span> <button class="delete-btn">x</button>
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `;
   listEl.appendChild(item);
 }
 
-// update the balance, income and expense
+// function to remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+// function to update the balance, income and expense
 function updateValues() {
   const amounts = transactions.map((transaction) => transaction.amount);
 
@@ -56,10 +103,14 @@ function updateValues() {
   moneyMinusEl.innerText = `$${expense}`;
 }
 
-// initialize app
+// function to initialize app
 function init() {
   listEl.innerHTML = "";
   transactions.forEach(addTransactionDom);
   updateValues();
 }
+
 init();
+
+// event listener
+formEl.addEventListener("submit", addTransaction);
