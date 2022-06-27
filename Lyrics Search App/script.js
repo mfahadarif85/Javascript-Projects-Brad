07ecfbb6b3a67dc7af6f8f6777c8f56ae30adf5e
mfahadarif85 <mfahadarif@gmail.com>
2022-06-27 14:00:08 +0500

    get & display song lyrics

--- Lyrics Search App/script.js
+++ Lyrics Search App/script.js
@@ -15,7 +15,53 @@ async function searchSongs(term) {
 
 //Show songs and artist in DOM
 function showData(data) {
-  return true;
+  result.innerHTML = `
+  <ul class="songs">
+    ${data.data
+      .map(
+        (song) => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+      )
+      .join("")}
+  </ul>
+  `;
+  // if (data.prev || data.next) {
+  //   more.innerHTML = `
+  //   ${
+  //     data.prev
+  //       ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+  //       : ""
+  //   }
+  //   ${
+  //     data.next
+  //       ? `<button class="btn" onclick="getMoreSongs('${data.next}')" >Next</button>`
+  //       : ""
+  //   }
+  //   `;
+  // } else {
+  //   more.innerHTML = "";
+  // }
+}
+
+// async function getMoreSongs(url) {
+//   const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+//   const data = await res.json();
+//   showData(data);
+// }
+
+// get lyrics for songs
+
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>
+  `;
+  more.innerHTML = "";
 }
 
 //Event Listeners
@@ -28,5 +74,17 @@ form.addEventListener("submit", (e) => {
   } else {
     errorText.style.opacity = 0;
     searchSongs(searchTerm);
+  }
+});
+
+// get lyrics button click
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
